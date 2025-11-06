93872060e537bcf74101bb8a60419ad8e1273bf1
alicegmn <alice.eriksson.561@gmail.com>
2025-11-06 08:38:44 +0100

    feat: add intentional error route for testing purposes

--- simple-server/index.js
+++ simple-server/index.js
@@ -35,7 +35,6 @@ app.get("/", (req, res) => {
 app.get("/notes", async (req, res) => {
 	const result = await pool.query("SELECT * FROM notes ORDER BY id DESC");
 	res.json(result.rows);
-	console.error("Här är ett fel! Men det är bara ett testfel.");
 });
 
 app.post("/notes", async (req, res) => {
@@ -45,6 +44,10 @@ app.post("/notes", async (req, res) => {
 		[text]
 	);
 	res.json(result.rows[0]);
+});
+
+app.get("/error", (req, res) => {
+	throw new Error("Intentional test error!");
 });
 
 app.listen(port, () => {
