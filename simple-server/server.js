a599d0e97be3399c9a6d2b207d044df8236e0af2
alicegmn <alice.eriksson.561@gmail.com>
2025-11-07 11:55:12 +0100

    ci: add second endpoint to confirm successful deployment

--- simple-server/server.js
+++ simple-server/server.js
@@ -76,4 +76,12 @@ app.get("/deployed", (req, res) => {
 	});
 });
 
+// second new endpoint showing us that deployment was succesful
+app.get("/deployed2", (req, res) => {
+	res.json({
+		message:
+			"The app has been successfully deployed 2 now so we can see the change!",
+	});
+});
+
 module.exports = app;
